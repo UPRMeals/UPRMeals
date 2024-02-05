9f4022f39b20037247ed48a57627e1b9782832d9
isabellagarrido <isabella.garrido@upr.edu>
2024-02-05 09:30:14 -0400

    Reformat previous files

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,10 +2,9 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
-import {  ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/styles/theme";
-import  CssBaseline  from "@mui/material/CssBaseline";
-
+import CssBaseline from "@mui/material/CssBaseline";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,15 +15,12 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-
-
- 
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return getLayout(
     <ThemeProvider theme={theme}>
       <CssBaseline />
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -16,33 +16,32 @@ const font = [
 ].join(",");
 
 let theme = createTheme({
-    palette: {
-      mode: "light",
-      background: {
-        default: "#f0f0f0", 
-      },
-      primary: cyan,
-      secondary: green,
+  palette: {
+    mode: "light",
+    background: {
+      default: "#f0f0f0",
     },
-    typography: {
-      fontFamily: font,
-      h1:{
-        fontWeight:700,
-      },
-      subtitle2: {
-        fontWeight: 600,
+    primary: cyan,
+    secondary: green,
+  },
+  typography: {
+    fontFamily: font,
+    h1: {
+      fontWeight: 700,
+    },
+    subtitle2: {
+      fontWeight: 600,
+    },
+  },
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        fontFamily: font,
+        color: "textPrimary",
       },
     },
-    components: {
-      MuiTypography: {
-        defaultProps: {
-          fontFamily: font,
-          color: "textPrimary",
-        },
-      },
-    },
-  
-  });
+  },
+});
 
 theme = responsiveFontSizes(theme);
 
