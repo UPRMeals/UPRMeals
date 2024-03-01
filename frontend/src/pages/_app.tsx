2fa809ec3a861433367413d9d9cb93e248ac4d5d
Kenneth-Aponte <kenneth.aponte2@upr.edu>
2024-03-01 18:14:36 -0400

    under /customer

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from "next";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/styles/theme";
 import CssBaseline from "@mui/material/CssBaseline";
-import Navbar from "./components/navbar";
+import Navbar from "./customers/components/navbar";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,64 +1,22 @@
-import { Box, Button, Typography, Stack, useTheme } from "@mui/material";
-import Image from "next/legacy/image";
-import Link from "next/link";
+import { Box, CircularProgress } from "@mui/material";
+import { useRouter } from "next/router";
 
 export default function HomePage() {
-  const theme = useTheme();
+  const router = useRouter();
+
+  if (router.isReady) {
+    router.push("/customers");
+  }
 
   return (
-    <Box mt={9} pb={10} px={5} alignItems={"center"}>
-      <Box
-        display="flex"
-        flexDirection={{ xs: "column", md: "row" }}
-        alignItems={"center"}
-      >
-        <Box
-          sx={{ position: "relative" }}
-          display={{ xs: "none", md: "block" }}
-        >
-          <Image
-            src="/Tarzan.png"
-            alt="Tarzan"
-            layout="fixed"
-            objectFit="contain"
-            height={713}
-            width={551}
-          />
-        </Box>
-        <Stack textAlign={"end"} alignItems="end">
-          <Typography variant="h1" fontFamily={"Bungee"}>
-            ¿La fila está larga?
-          </Typography>
-          <Typography variant="h3">Pídelo aquí.</Typography>
-          <Box
-            sx={{ position: "relative" }}
-            display={{ xs: "block", md: "none" }}
-            alignSelf="center"
-            mt={4}
-          >
-            <Image
-              src="/Tarzan.png"
-              alt="Tarzan"
-              layout="fixed"
-              objectFit="contain"
-              height={215}
-              width={173}
-            />
-          </Box>
-          <Stack
-            direction="row"
-            spacing={3}
-            mt={5}
-            alignSelf={{ xs: "center", md: "inherit" }}
-          >
-            <Button variant="contained">Order Now</Button>
-            <Link href="/menu">
-              <Button variant="outlined">View Menu</Button>
-            </Link>
-          </Stack>
-        </Stack>
-      </Box>
-      {/* <LoginPage /> */}
+    <Box
+      height={"100vh"}
+      width={"100vw"}
+      display={"flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      <CircularProgress size={80} />
     </Box>
   );
 }
