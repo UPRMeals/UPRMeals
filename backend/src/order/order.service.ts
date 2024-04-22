c9e34e2432d387b743679f7dded5fcee34b3437b
isaacruz2308 <155272270+isaacruz2308@users.noreply.github.com>
2024-04-22 01:35:35 -0400

    Quick fix

--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -64,7 +64,7 @@ export class OrderService {
         status: newStatus,
       },
     });
-    return { orderId: order.id };
+    return order;
   }
 
   async getAllOrdersForUser(userId: number): Promise<any> {
@@ -225,123 +225,5 @@ export class OrderService {
     });
 
     return ordersResponse; // Directly return the fetched data
-  }
-
-  async getAllOrdersForUser(userId: number): Promise<any> {
-    if (!userId) throw new Error('User not found');
-    const tempOrdersResponse = await this.prismaService.order.findMany({
-      where: {
-        userId: userId,
-        removed: false,
-      },
-      include: {
-        orderItems: {
-          include: {
-            item: true,
-          },
-        },
-        orderCombos: {
-          include: {
-            combo: true,
-            orderComboItems: {
-              include: {
-                item: true,
-              },
-            },
-          },
-        },
-      },
-    });
-
-    const orders = tempOrdersResponse.flatMap((order) => {
-      return this.getSimplifiedOrder(order);
-    });
-
-    return orders;
-  }
-
-  async getOrderById(orderId: number) {
-    if (!orderId) throw new Error('Method not implemented.');
-    const order = await this.prismaService.order.findUnique({
-      where: {
-        id: orderId,
-        removed: false,
-      },
-      include: {
-        orderItems: {
-          include: {
-            item: true,
-          },
-        },
-        orderCombos: {
-          include: {
-            combo: true,
-            orderComboItems: {
-              include: {
-                item: true,
-              },
-            },
-          },
-        },
-      },
-    });
-
-    return this.getSimplifiedOrder(order);
-  }
-
-  //gets rid of the relationships (easier for frontend)
-  private getSimplifiedOrder(order: PrismaFindOrderResponse): SimplifiedOrder {
-    const orderItems = order.orderItems.map((orderItem) => orderItem.item);
-
-    const orderCombos = order.orderCombos.map((orderCombo) => {
-      const proteins = orderCombo.orderComboItems.flatMap<Item>(
-        (orderComboItem) => {
-          return orderComboItem.item.type === ItemType.PROTEIN
-            ? orderComboItem.item
-            : [];
-        },
-      );
-
-      const sides = orderCombo.orderComboItems.flatMap<Item>(
-        (orderComboItem) => {
-          return orderComboItem.item.type === ItemType.SIDE
-            ? orderComboItem.item
-            : [];
-        },
-      );
-
-      return {
-        id: orderCombo.combo.id,
-        name: orderCombo.combo.name,
-        description: orderCombo.combo.description,
-        price: orderCombo.combo.price,
-        status: orderCombo.combo.status,
-        proteinCount: orderCombo.combo.proteinCount,
-        sideCount: orderCombo.combo.sideCount,
-        menuId: orderCombo.combo.menuId,
-        removed: orderCombo.combo.removed,
-        proteins,
-        sides,
-      };
-    });
-
-    return {
-      id: order.id,
-      userId: order.userId,
-      status: order.status,
-      totalPrice: order.totalPrice,
-      createdAt: new Date(order.createdAt).toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-      }),
-      updatedAt: new Date(order.updatedAt).toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-      }),
-      orderItems,
-      orderCombos,
-    };
   }
 }
