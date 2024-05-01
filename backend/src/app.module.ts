91afa53f71688b5e531d181892b38e1a21eeeed0
Kenneth-Aponte <kenneth.aponte2@upr.edu>
2024-04-30 21:24:23 -0400

    maybe is the fix?

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { UserController } from './user/user.controller';
 import { MenuModule } from './menu/menu.module';
-import { MenuController } from './menu/menu.controller';
 import { ItemModule } from './item/item.module';
 import { ComboModule } from './combo/combo.module';
 import { OrderModule } from './order/order.module';
-import { OrderController } from './order/order.controller';
-import { ItemController } from './item/item.controller';
-import { ComboController } from './combo/combo.controller';
 
 @Module({
   imports: [
@@ -27,15 +21,7 @@ import { ComboController } from './combo/combo.controller';
     ComboModule,
     OrderModule,
   ],
-  controllers: [
-    AppController,
-    AuthController,
-    UserController,
-    OrderController,
-    ItemController,
-    MenuController,
-    ComboController,
-  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
